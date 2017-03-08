7dea8bb4cb03dc88342513fc06d1429e0bf68b90
Tenkorang Osei Panin <paaqwazi@gmail.com>
2017-03-08 11:42:28 +0000

    Uncommitted Changes, committed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # adgeeklandingpage
+
+#<!--form action='https://script.google.com/macros/s/AKfycbz_gGsV4fARj3zJODyAPRvkW_SVBHu_FxvC4Wsi2AScnnKSW2Y/exec' -->
---
+++ php/notifyMe.php
@@ -0,0 +1,4 @@
+<?php
+if (isset($_POST['YourName'], $_POST['BusinessEmail'], $_POST['PhoneNumber'], $_POST['YourWebsite'], $_POST['FacebookPage'])){
+  print_r($_POST)
+}
